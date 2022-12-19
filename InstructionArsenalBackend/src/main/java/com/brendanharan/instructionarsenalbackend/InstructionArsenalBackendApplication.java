1049a8ea5609ab4e0bc8025499d8051f06454404
Brendan H <bjharan7@gmail.com>
2022-12-19 16:46:20 -0600

    repository setup

---
+++ InstructionArsenalBackend/src/main/java/com/brendanharan/instructionarsenalbackend/InstructionArsenalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brendanharan.instructionarsenalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructionArsenalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstructionArsenalBackendApplication.class, args);
+    }
+
+}
---
+++ InstructionArsenalBackend/src/test/java/com/brendanharan/instructionarsenalbackend/InstructionArsenalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendanharan.instructionarsenalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructionArsenalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
