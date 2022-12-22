ab0eff048eab02a28fa80658c51658b430d3ee5b
Brendan H <bjharan7@gmail.com>
2022-12-21 20:06:17 -0600

    beginnings of official instruction search

--- instruction_arsenal/lib/homepage/official_instructions/official_instructions_tab.dart
+++ instruction_arsenal/lib/homepage/official_instructions/official_instructions_tab.dart
@@ -23,7 +23,43 @@ class _OfficialInstructionsTabState extends State<OfficialInstructionsTab> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                width: MediaQuery.of(context).size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(Icons.search),
+                        ],
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.65,
+                        child: const ElevatedButton(onPressed: null, child: Text(
+                          "Search"
+                        ),
+                        ),
+                      )
+                      //filters and other stuff
+                    ],
+                  ),
+                )
+              )
 
             ],
           ),
