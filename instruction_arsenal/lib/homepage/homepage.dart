c2f7ff2de09303e1bcafc7facd18b86ba4bd672a
Brendan H <bjharan7@gmail.com>
2022-12-28 15:44:23 -0600

    added drawer but no real functionality yet

--- instruction_arsenal/lib/homepage/homepage.dart
+++ instruction_arsenal/lib/homepage/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instruction_arsenal/homepage/official_instructions/official_instructions_tab.dart';
 import 'package:instruction_arsenal/login_page/login_page.dart';
@@ -15,6 +16,7 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> with TickerProviderStateMixin{
 
   late TabController _tabController;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -31,9 +33,39 @@ class _HomepageState extends State<Homepage> with TickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(
+              height: 120.0,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                ),
+                child: Text(
+                  FirebaseAuth.instance.currentUser?.email ?? "Email",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: IconButton(icon: const Icon(Icons.account_circle_rounded, color: Colors.black), onPressed: () {  },),
+        leading: IconButton(icon: const Icon(Icons.account_circle_rounded, color: Colors.black), onPressed: () {_scaffoldKey.currentState!.openDrawer();}),
         centerTitle: true,
         title: const Text('Instruction Arsenal',
           style: TextStyle(
