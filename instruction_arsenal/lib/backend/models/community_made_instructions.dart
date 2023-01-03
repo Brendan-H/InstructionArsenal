6538434484ae1a6bb5df52877222c0e084969146
Brendan H <bjharan7@gmail.com>
2023-01-03 16:04:37 -0600

    dio work done so just needs to be implemented with listview

---
+++ instruction_arsenal/lib/backend/models/community_made_instructions.dart
@@ -0,0 +1,132 @@
+/// id : 1
+/// title : "Enable Developer Mode on Android"
+/// description : "Enabling Developer Mode on Android 13 for access to developer settings"
+/// postCreatedAt : "2023-01-02T21:11:45.280416"
+/// instructions : "1. Open Settings, 2. Open About Phone, 3. Click Android Version, 4. Click Build Number 7 Times, 5. Navigate to Settings/System/Developer Options"
+/// createdBy : "bjharan7@gmail.com"
+/// category : "Technology"
+/// likes : 5
+/// dislikes : 2
+/// tags : "Android, settings, developer mode"
+/// difficulty : "Easy"
+/// timeToComplete : "2 Minutes"
+/// sponsored : false
+
+class CommunityMadeInstructions {
+  CommunityMadeInstructions({
+      num? id, 
+      String? title, 
+      String? description, 
+      String? postCreatedAt, 
+      String? instructions, 
+      String? createdBy, 
+      String? category, 
+      num? likes, 
+      num? dislikes, 
+      String? tags, 
+      String? difficulty, 
+      String? timeToComplete, 
+      bool? sponsored,}){
+    _id = id;
+    _title = title;
+    _description = description;
+    _postCreatedAt = postCreatedAt;
+    _instructions = instructions;
+    _createdBy = createdBy;
+    _category = category;
+    _likes = likes;
+    _dislikes = dislikes;
+    _tags = tags;
+    _difficulty = difficulty;
+    _timeToComplete = timeToComplete;
+    _sponsored = sponsored;
+}
+
+  CommunityMadeInstructions.fromJson(dynamic json) {
+    _id = json['id'];
+    _title = json['title'];
+    _description = json['description'];
+    _postCreatedAt = json['postCreatedAt'];
+    _instructions = json['instructions'];
+    _createdBy = json['createdBy'];
+    _category = json['category'];
+    _likes = json['likes'];
+    _dislikes = json['dislikes'];
+    _tags = json['tags'];
+    _difficulty = json['difficulty'];
+    _timeToComplete = json['timeToComplete'];
+    _sponsored = json['sponsored'];
+  }
+  num? _id;
+  String? _title;
+  String? _description;
+  String? _postCreatedAt;
+  String? _instructions;
+  String? _createdBy;
+  String? _category;
+  num? _likes;
+  num? _dislikes;
+  String? _tags;
+  String? _difficulty;
+  String? _timeToComplete;
+  bool? _sponsored;
+CommunityMadeInstructions copyWith({  num? id,
+  String? title,
+  String? description,
+  String? postCreatedAt,
+  String? instructions,
+  String? createdBy,
+  String? category,
+  num? likes,
+  num? dislikes,
+  String? tags,
+  String? difficulty,
+  String? timeToComplete,
+  bool? sponsored,
+}) => CommunityMadeInstructions(  id: id ?? _id,
+  title: title ?? _title,
+  description: description ?? _description,
+  postCreatedAt: postCreatedAt ?? _postCreatedAt,
+  instructions: instructions ?? _instructions,
+  createdBy: createdBy ?? _createdBy,
+  category: category ?? _category,
+  likes: likes ?? _likes,
+  dislikes: dislikes ?? _dislikes,
+  tags: tags ?? _tags,
+  difficulty: difficulty ?? _difficulty,
+  timeToComplete: timeToComplete ?? _timeToComplete,
+  sponsored: sponsored ?? _sponsored,
+);
+  num? get id => _id;
+  String? get title => _title;
+  String? get description => _description;
+  String? get postCreatedAt => _postCreatedAt;
+  String? get instructions => _instructions;
+  String? get createdBy => _createdBy;
+  String? get category => _category;
+  num? get likes => _likes;
+  num? get dislikes => _dislikes;
+  String? get tags => _tags;
+  String? get difficulty => _difficulty;
+  String? get timeToComplete => _timeToComplete;
+  bool? get sponsored => _sponsored;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = _id;
+    map['title'] = _title;
+    map['description'] = _description;
+    map['postCreatedAt'] = _postCreatedAt;
+    map['instructions'] = _instructions;
+    map['createdBy'] = _createdBy;
+    map['category'] = _category;
+    map['likes'] = _likes;
+    map['dislikes'] = _dislikes;
+    map['tags'] = _tags;
+    map['difficulty'] = _difficulty;
+    map['timeToComplete'] = _timeToComplete;
+    map['sponsored'] = _sponsored;
+    return map;
+  }
+
+}
--- instruction_arsenal/lib/homepage/community_made_instructions/community_made_instructions_tab.dart
+++ instruction_arsenal/lib/homepage/community_made_instructions/community_made_instructions_tab.dart
@@ -1,4 +1,8 @@
+import 'package:awesome_select/awesome_select.dart';
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instruction_arsenal/backend/models/community_made_instructions.dart';
 
 class CommunityMadeInstructionsTab extends StatefulWidget {
   const CommunityMadeInstructionsTab({Key? key}) : super(key: key);
@@ -8,6 +12,138 @@ class CommunityMadeInstructionsTab extends StatefulWidget {
 }
 
 class _CommunityMadeInstructionsTabState extends State<CommunityMadeInstructionsTab> {
+  final TextEditingController _searchController = TextEditingController();
+
+  Future<List<CommunityMadeInstructions>?> fetchCommunityMadeInstructions() async {
+    var idToken = await FirebaseAuth.instance.currentUser!.getIdToken();
+    var dio = Dio();
+    var title = _searchController.text;
+    var category = categoryChoice;
+    if (_searchController.text.isNotEmpty) {
+      var response = await dio.get('http://10.0.2.2:8080/api/v1/instructions/communitymadeinstructions/titleandcategory/$title/$category',
+          options: Options(
+            headers: {
+              'Authorization': "Bearer $idToken",
+            },
+          ));
+      if (response.statusCode == 200) {
+        var communityMadeInstructions = <CommunityMadeInstructions>[];
+        for (var communityMadeInstructionsJson in response.data) {
+          communityMadeInstructions.add(CommunityMadeInstructions.fromJson(communityMadeInstructionsJson));
+        }
+        print(communityMadeInstructions);
+        return communityMadeInstructions;
+
+      }
+      else {
+        if (response.statusCode == 404) {
+          print('404');
+        }
+        throw Exception('An error occurred');
+      }
+    } else if (_searchController.text.isEmpty) {
+      var response = await dio.get('http://10.0.2.2:8080/api/v1/instructions/communitymadeinstructions/all',
+          options: Options(
+            headers: {
+              'Authorization': "Bearer $idToken",
+            },
+          )
+      );
+      if (response.statusCode == 200) {
+        var communityMadeInstructions = <CommunityMadeInstructions>[];
+        for (var communityMadeInstructionsJson in response.data) {
+          communityMadeInstructions.add(CommunityMadeInstructions.fromJson(communityMadeInstructionsJson));
+        }
+        print(communityMadeInstructions);
+        return communityMadeInstructions;
+
+      }
+      else {
+        if (response.statusCode == 404) {
+          print('404');
+        }
+        throw Exception('An error occurred');
+      }
+    } else {
+      return null;
+    }
+
+  }
+  showAlertDialog(BuildContext context) {
+
+    // set up the buttons
+    Widget cancelButton = TextButton(
+      child: const Text("Cancel"),
+      onPressed:  () async {Navigator.pop(context);},
+    );
+    Widget continueButton = TextButton(
+      child: const Text("Continue"),
+      onPressed:  () async {
+        Navigator.pop(context);
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Post Deleted")));
+        Navigator.pop(context);
+
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text("Delete Post"),
+      content: const Text("Are you sure you would like to delete this post?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  List<S2Choice<String>> categoryChoiceOptions = [
+    S2Choice<String>(value: 'Technology', title: 'Technology'),
+    S2Choice<String>(value: 'Cars/Automotive', title: 'Cars/Automotive'),
+    S2Choice<String>(value: 'Cooking', title: 'Cooking'),
+    S2Choice<String>(value: 'Sports', title: 'Sports'),
+    S2Choice<String>(value: 'Home', title: 'Home'),
+    S2Choice<String>(value: 'Other', title: 'Other'),
+  ];
+
+  late Future<List<CommunityMadeInstructions>?> futureCommunityMadeInstructions;
+
+  bool _buttonPressed = false;
+
+  var categoryChoice = "other";
+
+  @override
+  void initState() {
+    super.initState();
+    futureCommunityMadeInstructions = fetchCommunityMadeInstructions();
+  }
+
+  void submitValue(String value) async {
+    if (_searchController.text.length <
+        4) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(
+        const SnackBar(
+          content: Text(
+            "Title must be at least 4 characters long!",
+          ),
+        ),
+      );
+      return;
+    }
+    fetchCommunityMadeInstructions();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +154,33 @@ class _CommunityMadeInstructionsTabState extends State<CommunityMadeInstructions
       width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                child: SmartSelect<String>.single(
+                  choiceStyle: const S2ChoiceStyle(
+                    titleStyle: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  onSelect: (selected, state) {
+                    print(categoryChoice);
+                    fetchCommunityMadeInstructions();
+                  },
+                  title: 'Category',
+                  selectedValue: categoryChoice,
+                  choiceItems: categoryChoiceOptions,
+                  onChange: (state) => setState(() {
+                    categoryChoice = state.value;
+                  }),
+                ),
+              ),
               TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  submitValue(value);
+                },
                 onSubmitted: (value) {
-                  print(value);
+                  submitValue(value);
                   //Refresh results
                 },
                 decoration: InputDecoration(
@@ -31,41 +191,10 @@ class _CommunityMadeInstructionsTabState extends State<CommunityMadeInstructions
                   ),
                 ),
               ),
-              Card(
-                elevation: 2,
-                child: ListTile(
-                  leading: const Icon(Icons.phone_android),
-                  //TODO icon changes based on the category (ex. Tech, Food, etc.)
-                  //TODO the listview is always shown with http://.../communitymade/all but changes once a search is made
-                  title: const Text('How to make a phone call'),
-                  subtitle: const Text('Example Search Result Description'),
-                  onTap: () {
-                    //Search for instructions
-                  },
-                ),
-              ),
-              Card(
-                elevation: 2,
-                child: ListTile(
-                  leading: const Icon(Icons.fastfood),
-                  title: const Text('How to cook a steak'),
-                  subtitle: const Text('Example Search Result Description'),
-                  onTap: () {
-                    //Search for instructions
-                  },
-                ),
-              ),
-              Card(
-                elevation: 2,
-                child: ListTile(
-                  leading: const Icon(Icons.directions_car),
-                  title: const Text('How to change a tire'),
-                  subtitle: const Text('Example Search Result Description'),
-                  onTap: () {
-                    //Search for instructions
-                  },
-                ),
-              ),
+
+              //TODO icon changes based on the category (ex. Tech, Food, etc.)
+              //TODO the listview is always shown with http://.../communitymade/all but changes once a search is made
+
               Container(
                 width: MediaQuery.of(context).size.width * .9945,
                 height: MediaQuery.of(context).size.height * .33,
@@ -79,10 +208,10 @@ class _CommunityMadeInstructionsTabState extends State<CommunityMadeInstructions
                         Padding(
                           padding: const EdgeInsets.fromLTRB(0, 2, 0, 5),
                           child: Row(
-                            children: [
-                              const Text("Created By: bjharan7@gmail.com"),
-                              const Spacer(),
-                              const Text("Sponsored",
+                            children: const [
+                              Text("Created By: bjharan7@gmail.com"),
+                              Spacer(),
+                              Text("Sponsored",
                               style: TextStyle(
                                       color: Colors.blue,
                                       fontWeight: FontWeight.bold)
@@ -95,16 +224,16 @@ class _CommunityMadeInstructionsTabState extends State<CommunityMadeInstructions
                           fontSize: 22,
                           fontWeight: FontWeight.w600
                         ),),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                          child: const Text("This tutorial will show you the exact steps for safely replacing the tire on any car. It will be demonstrated on a 2017 Toyota Camry",
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                          child: Text("This tutorial will show you the exact steps for safely replacing the tire on any car. It will be demonstrated on a 2017 Toyota Camry",
                           style: TextStyle(
                             fontSize: 15,
                           ),),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                          child: const Text("""Step 1: Park the car on a flat surface and turn off the engine. Make sure the car is in park and the parking brake is on.
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                          child: Text("""Step 1: Park the car on a flat surface and turn off the engine. Make sure the car is in park and the parking brake is on.
                            Step 2: Locate the jack and the lug wrench. Step 3: Remove the lug nuts from the tire . . .
                           """,
                             //TODO only show first 100 characters and add "..." at the end
@@ -165,9 +294,3 @@ class _CommunityMadeInstructionsTabState extends State<CommunityMadeInstructions
 // this.difficulty = difficulty;
 // this.timeToComplete = timeToComplete;
 // this.isSponsored = isSponsored;
-
-//
-// Step 4: Remove the tire from the car. Step 5: Place the spare tire on the car.
-// Step 6: Replace the lug nuts and tighten them. Step 7: Lower the car and remove the jack.
-// Step 8: Tighten the lug nuts. Step 9: Put the jack and lug wrench back in the trunk.
-// Step 10: Drive the car to a tire shop to get the flat tire fixed.
